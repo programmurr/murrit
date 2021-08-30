625948f51ceb20b24d074a4244b95ce2da5bd66c
conor <conorjm89@gmail.com>
2021-08-30 21:20:53 +0100

    Users collection gets update with post ID with Cloud Function

---
+++ functions/index.js
@@ -0,0 +1,29 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.writeToFirestore = functions.firestore
+    .document("posts/{postId}")
+    .onCreate((snap, context) => {
+      const newPost = snap.data();
+      const authorId = newPost.author;
+      const newPostId = context.params.postId;
+      const userRef = db.collection("users").doc(`${authorId}`);
+      userRef.get()
+          .then((doc) => {
+            const userData = doc.data();
+            const newPosts = [...userData.posts, newPostId];
+            db.collection("users").doc(`${authorId}`).update({
+              posts: newPosts,
+            })
+                .then(() => {
+                  console.log("Users successfully updated");
+                });
+          })
+          .catch((error) => {
+            console.log("Error getting user document: ", error);
+          });
+    });
+
